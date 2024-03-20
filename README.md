88aad6b316c7f8e17c7a0a336ae28d0eaf3bfd50
yplam <yplam@yplam.com>
2024-03-20 12:15:54 +0800

    add 3dpack, ootd, flowty crm

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Run multiple instances ComfyUI using Docker
 
-```
+Note: only tested in linux with nvidia gpu
 
-./scripts/download.sh base
-./scripts/download.sh comfyui
+Simple usage:
+
+``` bash
+./scripts/download.sh ./build/base
+./scripts/download.sh ./build/comfyui
 docker compose --profile builder build
 docker compose --profile comfyui up
 ```
+
+Support profiles:
+
+* comfyui: common profile for basic workflow
+* ootd: [OOTDiffusion](https://github.com/AuroBit/ComfyUI-OOTDiffusion)
+* 3dpack: [ComfyUI-3D-Pack](https://github.com/MrForExample/ComfyUI-3D-Pack)
+* flowty: [ComfyUI-Flowty-CRM](https://github.com/flowtyone/ComfyUI-Flowty-CRM)
--- scripts/download.sh
+++ scripts/download.sh
@@ -15,13 +15,22 @@ readarray base_nodes < base
 readarray instance_nodes < $instance_name
 
 cd ./instances
-rm -rf $instance_name
+
+if [ -d $instance_name ]; then
+    echo "Instance already exists"
+    exit 1
+fi
+
 git clone https://github.com/comfyanonymous/ComfyUI.git $instance_name
+
+mkdir -p $instance_name/.cache/huggingface
+mkdir -p $instance_name/pysssss-workflows
+
 cd $instance_name/custom_nodes
 
 for i in "${instance_nodes[@]}"
 do
-    git clone $i
+    git clone --depth=1 $i
 done
 
 node_dirs=$(ls -d */)
@@ -29,9 +38,14 @@ node_dirs=$(ls -d */)
 if [ "$instance_name" != "base" ]; then
     for i in "${base_nodes[@]}"
     do
-        git clone $i
+        git clone --depth=1 $i
     done
 fi
+
+if [ "$instance_name" == "3dpack" ]; then
+    mkdir -p ./ComfyUI-3D-Pack/checkpoints
+fi
+
 
 cd ../../../
 
@@ -45,6 +59,13 @@ fi
 for i in $node_dirs
 do
     echo $i
-    mkdir "./build/requirements_$instance_name/$i"
-    cp "./instances/$instance_name/custom_nodes/${i}requirements.txt" "./build/requirements_$instance_name/$i"
+    source_dir="./instances/$instance_name/custom_nodes/$i"
+    target_dir="./build/requirements_$instance_name/$i"
+    mkdir $target_dir
+    for file in "${source_dir}requirements.txt" "${source_dir}requirements-cuda.txt"; do
+        if [ -f "$file" ]; then
+            echo $file
+            cp $file $target_dir
+        fi
+    done
 done
